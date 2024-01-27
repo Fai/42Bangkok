7f2c78645214a9c92fc671006e3b1e2632f03fea
Ruchida Pithaksiripan <rpithaks@z2t4c3.42bangkok.com>
2024-01-28 05:06:38 +0700

    feat: ft_strdup, ft_strjoin, ft_split, ft_convert_base, ft_ultimate_range

--- dev/c10/ex01/srcs/ft_cat.c
+++ dev/c10/ex01/srcs/ft_cat.c
@@ -6,7 +6,30 @@
 /*   By: rpithaks <rpithaks@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 12:15:04 by rpithaks          #+#    #+#             */
-/*   Updated: 2024/01/26 23:04:28 by rpithaks         ###   ########.fr       */
+/*   Updated: 2024/01/28 04:20:55 by rpithaks         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+#include <unistd.h>
+#include "sys/stat.h"
+#include "sys/types.h"
+#include <fcntl.h>
+
+int	main(int ac, char **av)
+{
+	int		fd;
+	char	c;
+
+	if (ac < 2)
+		return (write(2, "File name missing.\n", 19));
+	if (ac > 2)
+		return (write(2, "Too many arguments.\n", 20));
+	fd = open(av[1], O_RDONLY);
+	if (fd < 0)
+		return (write(2, "Cannot read file.\n", 18));
+	while (read(fd, &c, 1))
+		write(1, &c, 1);
+	close(fd);
+	return (0);
+}
